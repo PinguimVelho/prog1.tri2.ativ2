a5c5b5d7cf5efff7d961713c63d230c2581409b4
Your Name <you@example.com>
2026-06-03 16:41:00 -0300

    aula 1 server

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
+// FALAR SOBRE API REST
+
 const server = Bun.serve({
     port: 1337,
     routes: {
-        "/retornaok": new Response("Ok"),
-        "/retornadunha": new Response("Dunhanada"),
-        "/pghtml": Bun.file("public/index.html")
+        "/api-debugger": Bun.file('public/api-debugger.html'),
+        "/test": {
+            GET: () => Response.json({ time: Date.now() }),
+            PUT: () => Response.json({ time: Date.now() }),
+            POST: () => Response.json({ time: Date.now() }),
+            DELETE: () => Response.json({ time: Date.now() }),
+        }
     },
-
-    fetch(req) {
-        return new Response("404")    
+    fetch() {
+        return new Response("Not Found", { status:404 })
     }
 })
-console.log(`Servervidor rodando em ${server.url}`);
+
+console.log(`⚡ Servervidor rodando em ${server.url}`);
